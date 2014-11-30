f89c469479b24acfda4f79335ac2b0883863f2a9
geppettodivacin <ericdilmore@gmail.com>
2014-11-29 20:38:55 -0600

    Remove the improper grid movement in printgrid.

--- main.asm
+++ main.asm
@@ -96,7 +96,6 @@ printgrid:
  g_loop:
 	beq  	$t1, $t3, g_end		# exit if the current position is the last position in the grid
 	beq  	$t0, $t2, g_newRow	# move to the next line if the current position is the last position in the row
-	mul 	$t4, $t1, 4 		# multiply $t1 by 4 to get the offset within the array
 	add 	$t4, $s6, $t4 		# add base address of array to $t4 to calculate the address of array[$t4]
 	lw 	$t4, 0($t4) 		# $t4 = array[$t4]
 	move 	$a0, $t4		# move the value to $a0
